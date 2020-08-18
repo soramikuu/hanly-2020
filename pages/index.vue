79c8d8052a113402aea61c1fc6302cfdfb19fa8b
soramikuu <soramikuu72@gmail.com>
2020-08-18 12:09:30 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <div>
+      <logo />
+      <h1 class="title">hanly</h1>
+      <h2 class="subtitle">こっちにも「{{ txt }}」</h2>
+      <div class="links">
+        <nuxt-link to="/test" class="button--green">テストページへ</nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  },
+  computed: {
+    ...mapGetters('test', ['txt'])
+  }
+}
+</script>
